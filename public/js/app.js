f0af179f946696614e471d6f11d71b117fb0851f
Susan Eldridge <seldridge@csolsus.local>
2021-12-13 23:59:28 -0500

    rmv unneeded line, fix const error

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
-console.log('Client side javascript is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+var messageOne = document.querySelector('#message-1')
+var messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
